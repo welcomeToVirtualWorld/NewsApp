d3b286df88c5bda902c8b157a3ddda95607630d3
Ashutosh Mishra <Ashutosh.Mishra@sportzinteractive.com>
2022-08-11 14:53:38 +0530

    Favourite Issue

--- app/src/main/java/com/example/newsapp/ui/models/Article.kt
+++ app/src/main/java/com/example/newsapp/ui/models/Article.kt
@@ -9,7 +9,7 @@ import java.io.Serializable
 data class Article(
     @PrimaryKey(autoGenerate = true)
     var id:Int?=null,
-    val author: String,
+    val author: String="",
     val content: String,
     val description: String,
     val publishedAt: String,
